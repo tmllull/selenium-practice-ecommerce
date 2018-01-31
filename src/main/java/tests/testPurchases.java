2363a37f903d104867de93e1e1564fd0528942f6
Toni Miquel <netyaco@gmail.com>
2018-01-31 11:02:12 +0100

    Testing jenkins trigger

--- src/main/java/tests/testPurchases.java
+++ src/main/java/tests/testPurchases.java
@@ -45,7 +45,7 @@ public class testPurchases {
 	@Test
 	public void test() throws Exception {
 		List<PurchaseItem> items=new LinkedList();
-		items.add(new PurchaseItem("Blouse",2));
+		items.add(new PurchaseItem("Blouse",3));
 		items.add(new PurchaseItem("Printed Chiffon Dress",1));
 		PurchaseProcess purchase=new PurchaseProcess(driver,items);
 		purchase.execute();
